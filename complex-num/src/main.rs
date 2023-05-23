06f23b1e0e8c46a0dcd26cd906139e9a4765ed20
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-05-23 17:14:16 +0800

    表达式

--- complex-num/src/main.rs
+++ complex-num/src/main.rs
@@ -5,5 +5,14 @@ fn main() {
     let b = Complex::new(11.1, 22.2);
     let result = a + b;
 
-    println!("{} + {}i", result.re, result.im)
+    println!("{} + {}i", result.re, result.im);
+
+    let a = add_with_extra(1, 2);
+    println!("a = {}", a);
+}
+
+fn add_with_extra(x: i32, y: i32) -> i32 {
+    let x = x + 1; // 语句
+    let y = y + 5; // 语句
+    x + y // 表达式
 }
