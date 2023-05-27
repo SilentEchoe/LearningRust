deb3c792ec1fd3fb5d952474b41161e5a2abb048
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-05-27 20:22:53 +0800

    深拷贝

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s);
+    let x: &str = "hello, world";
+    let y = x;
+    println!("{},{}", x, y);
 
     let s1 = String::from("hello");
-    let s2 = s1;
-    println!("{}, world!", s1);
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
