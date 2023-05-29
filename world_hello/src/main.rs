f6600c8d674b4bcfe005a0a572edd89e72e3ba5d
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-05-29 10:06:25 +0800

    可变引用

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,23 +1,8 @@
 fn main() {
-    let s = String::from("hello"); // s 进入作用域
-
-    takes_ownership(s); // s 的值移动到函数里 ...
-                        // ... 所以到这里不再有效
-
-    let x = 5; // x 进入作用域
-
-    makes_copy(x); // x 应该移动函数里，
-                   // 但 i32 是 Copy 的，所以在后面可继续使用 x
-
-    println!("在move进函数后继续使用s: {}", s);
+    let mut s = String::from("hello");
+    change(&mut s);
 }
 
-fn takes_ownership(some_string: String) {
-    // some_string 进入作用域
-    println!("{}", some_string);
-} // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
-
-fn makes_copy(some_integer: i32) {
-    // some_integer 进入作用域
-    println!("{}", some_integer);
-} // 这里，some_integer 移出作用域。不会有特殊操作
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
