560b470e3fbf985294d1f499585c46d3dbfb8dab
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-05-30 14:32:29 +0800

    String 转换 &str

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let reference_to_nothing = dangle();
+    let s = String::from("hello");
+    say_hello(&s);
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
-
-    &s
+fn say_hello(s: &str) {
+    println!("{}", s);
 }
