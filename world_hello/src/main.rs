a23215fe84891d041d77d2060221796e3e4ecbdf
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-06-04 22:18:50 +0800

    数组

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,34 +1,5 @@
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("another@example.com"),
-        sign_in_count: user1.sign_in_count,
-    };
-    println!("{}", user1.active);
-    // 下面这行会报错
-    println!("{:?}", user1);
-}
+    let array: [String; 8] = std::array::from_fn(|i| String::from("rust is good!"));
 
-#[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+    println!("{:#?}", array);
 }
