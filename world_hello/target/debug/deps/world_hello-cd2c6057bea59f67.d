576cada25dbb5886af03636064d741f675761121
anonymity <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-06-09 10:23:47 +0800

    Delete world_hello/target directory

--- world_hello/target/debug/deps/world_hello-cd2c6057bea59f67.d
+++
@@ -1,5 +0,0 @@
-/Users/kai/PrivateProject/src/LearningRust/world_hello/target/debug/deps/world_hello-cd2c6057bea59f67.rmeta: src/main.rs
-
-/Users/kai/PrivateProject/src/LearningRust/world_hello/target/debug/deps/world_hello-cd2c6057bea59f67.d: src/main.rs
-
-src/main.rs:
