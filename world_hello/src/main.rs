efc789f64605d66a9f074983ecdd804b7ef6a2ef
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-06-25 16:18:36 +0800

    method demo

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,28 +1,21 @@
-fn main() {}
-
-struct Circle {
-    x: f64,
-    y: f64,
-    radius: f64,
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-impl Circle {
-    // new 是Circle的关联函数,因为它的第一个参数不是self,且 new并不是关键字
-    // 这种方法往往用于初始化当前结构体的实例
-
-    fn new(x:f64,y:f64,radius:f64) -> Circle {
-        Circle {
-            x: x,
-            y: y,
-            radius: radius,
-        }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
-
-    fn area(&self) -> f64 {
-        std::f64::consts::PI * (self.radius * self.radius)
-    }
-        
 }
 
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
-
