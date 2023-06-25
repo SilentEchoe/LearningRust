b76b5fe02ac222e142e735b2aff1ff01337c80db
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-06-25 15:37:53 +0800

    方法

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,17 +1,28 @@
-fn main() {
-    let p = Point { x: 0, y: 7 };
-    let Point { x: a, y: b } = p;
-    assert_eq!(0, a);
-    assert_eq!(7, b);
+fn main() {}
 
-    match p {
-        Point { x, y: 0 } => println!("On the x axis at {}", x),
-        Point { x: 0, y } => println!("On the y axis at {}", y),
-        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl Circle {
+    // new 是Circle的关联函数,因为它的第一个参数不是self,且 new并不是关键字
+    // 这种方法往往用于初始化当前结构体的实例
+
+    fn new(x:f64,y:f64,radius:f64) -> Circle {
+        Circle {
+            x: x,
+            y: y,
+            radius: radius,
+        }
     }
+
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+        
 }
 
-struct Point {
-    x: i32,
-    y: i32,
 }
+
