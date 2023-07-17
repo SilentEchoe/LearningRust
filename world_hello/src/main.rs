513890f566a54569a74815c2dacc75b1261229a2
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-07-17 17:53:39 +0800

    泛形

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,36 +1,7 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn width(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn new(w: u32, h: u32) -> Rectangle {
-        Rectangle {
-            width: w,
-            height: h,
-        }
-    }
-}
-
-impl Rectangle {
-    fn height(&self) -> u32 {
-        self.height
-    }
+fn add<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    a + b
 }
 
 fn main() {
-    let rect1 = Rectangle::new(30, 50);
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.width(),
-    );
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.height(),
-    );
+    println!("{}", add(1, 2));
 }
