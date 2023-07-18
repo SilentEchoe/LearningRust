704c7b8a6411627c8c3d918ecd3d6762219a5ef0
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-07-18 17:21:48 +0800

    编写练习题

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,24 +1,20 @@
-struct Point<T> {
+// 实现下面的泛型函数 sum
+fn sum<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+struct Point<T, U> {
     x: T,
-    y: T,
-}
-
-enum Option<T> {
-    Some(T),
-    None,
-}
-
-fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
-    println!("arr = {:?}", arr);
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    println!("integer.x = {}", integer.x);
-
-    let arr: [i32; 3] = [1, 2, 3];
-    display_array(arr);
-
-    let arr: [i32; 2] = [4, 5];
-    display_array(arr);
+    assert_eq!(5, sum(2i8, 3i8));
+    assert_eq!(50, sum(20, 30));
+    assert_eq!(2.46, sum(1.23, 1.23));
+    let p = Point {
+        x: 5,
+        y: "hello".to_string(),
+    };
+    println!("p.x = {}, p.y = {}", p.x, p.y)
 }
