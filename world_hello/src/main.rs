0754da55865f6be7bb74395273dc892bf3b94958
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-07-18 17:00:43 +0800

    const 泛型

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -3,7 +3,22 @@ struct Point<T> {
     y: T,
 }
 
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn display_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+    println!("arr = {:?}", arr);
+}
+
 fn main() {
     let integer = Point { x: 5, y: 10 };
     println!("integer.x = {}", integer.x);
+
+    let arr: [i32; 3] = [1, 2, 3];
+    display_array(arr);
+
+    let arr: [i32; 2] = [4, 5];
+    display_array(arr);
 }
