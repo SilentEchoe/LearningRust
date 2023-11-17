af22b19ccb7cbc898152e88e98b87ff7cf1cc404
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-11-17 17:24:45 +0800

    特征对象

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,76 +1,53 @@
-use std::path::Component;
-
-// 为UI组件定义一个特性
-pub trait Draw {
-    fn draw(&self);
-}
-
-//只要组件实现了Draw 特征，就可以调用 draw 方法来进行渲染。
-
-// 按钮
-pub struct Button {
-    pub width: u32,
-    pub hight: u32,
-    pub label: String,
-}
-
-// Button实现 Draw 特征
-impl Draw for Button {
-    fn draw(&self) {
-        // 绘制按钮
-        println!("{}", "绘制按钮")
-    }
-}
-
-struct SelectBox {
-    pub width: u32,
-    pub hight: u32,
-    pub options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        // 绘制SelectBox的代码
-        println!("{}", "绘制SelectBox")
-    }
-}
-
-// 动态数组存储UI对象
-
-pub struct Screen<T: Draw> {
-    pub components: Vec<T>,
-}
-
-impl<T> Screen<T>
-where
-    T: Draw,
-{
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
-        }
-    }
-}
+use std::string;
 
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
+    let storage = StorageLayer {
+        StorageComponents: vec![Box::new(Mysql {
+            Host: String::from("Mysql"),
+        })],
     };
+    storage.run()
+}
 
-    screen.run();
+// 定义一个存储类型的特征
+pub trait Storage {
+    // 定义一个链接的行为
+    fn Connen(&self);
+}
+
+// 定义一个Mysql结构体
+pub struct Mysql {
+    pub Host: String,
+}
+
+//Mysql 实现存储层的连接操作
+impl Storage for Mysql {
+    fn Connen(&self) {
+        println!("{}", "Mysql 连接成功")
+    }
+}
+
+// 定义一个MongoDB的结构体
+pub struct MongoDB {
+    pub Host: String,
+}
+
+impl Storage for MongoDB {
+    fn Connen(&self) {
+        println!("{}", "MongoDB 连接成功")
+    }
+}
+
+//创建一个统一的动态数组存储这些连接
+
+pub struct StorageLayer {
+    pub StorageComponents: Vec<Box<dyn Storage>>,
+}
+
+impl StorageLayer {
+    pub fn run(&self) {
+        for component in self.StorageComponents.iter() {
+            component.Connen()
+        }
+    }
 }
