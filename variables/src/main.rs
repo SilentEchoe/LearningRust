15ad20bf665de5f78ccd5eb8253096a2aea14742
anonymity <kai.wang.info@gmail.com>
2024-01-31 14:03:22 +0800

    delete vscode

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,10 +1,15 @@
-use std::string;
+#[warn(unused_imports)]
 
 fn main() {
     let storage = StorageLayer {
-        StorageComponents: vec![Box::new(Mysql {
-            Host: String::from("Mysql"),
-        })],
+        storage_components: vec![
+            Box::new(Mysql {
+                host: String::from("Mysql Address"),
+            }),
+            Box::new(MongoDB {
+                host: String::from("MongoDB Address"),
+            }),
+        ],
     };
     storage.run()
 }
@@ -12,28 +17,28 @@ fn main() {
 // 定义一个存储类型的特征
 pub trait Storage {
     // 定义一个链接的行为
-    fn Connen(&self);
+    fn connen(&self);
 }
 
 // 定义一个Mysql结构体
 pub struct Mysql {
-    pub Host: String,
+    pub host: String,
 }
 
 //Mysql 实现存储层的连接操作
 impl Storage for Mysql {
-    fn Connen(&self) {
+    fn connen(&self) {
         println!("{}", "Mysql 连接成功")
     }
 }
 
 // 定义一个MongoDB的结构体
 pub struct MongoDB {
-    pub Host: String,
+    pub host: String,
 }
 
 impl Storage for MongoDB {
-    fn Connen(&self) {
+    fn connen(&self) {
         println!("{}", "MongoDB 连接成功")
     }
 }
@@ -41,13 +46,13 @@ impl Storage for MongoDB {
 //创建一个统一的动态数组存储这些连接
 
 pub struct StorageLayer {
-    pub StorageComponents: Vec<Box<dyn Storage>>,
+    pub storage_components: Vec<Box<dyn Storage>>,
 }
 
 impl StorageLayer {
     pub fn run(&self) {
-        for component in self.StorageComponents.iter() {
-            component.Connen()
+        for component in self.storage_components.iter() {
+            component.connen()
         }
     }
 }
