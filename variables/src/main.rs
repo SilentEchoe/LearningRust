6e86e25f0d1fb6eabe2c796f64e64a1b2e7c7dac
anonymity <kai.wang.info@gmail.com>
2024-02-01 15:09:56 +0800

    Ownership

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,58 +1,12 @@
 #[warn(unused_imports)]
 
+// 不要修改 main 中的代码
 fn main() {
-    let storage = StorageLayer {
-        storage_components: vec![
-            Box::new(Mysql {
-                host: String::from("Mysql Address"),
-            }),
-            Box::new(MongoDB {
-                host: String::from("MongoDB Address"),
-            }),
-        ],
-    };
-    storage.run()
+    let t = (String::from("hello"), String::from("world"));
+
+    // 填空，不要修改其它代码
+    let (s1 ,s2) = (&t.0 ,&t.1);
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
 }
 
-// 定义一个存储类型的特征
-pub trait Storage {
-    // 定义一个链接的行为
-    fn connen(&self);
-}
-
-// 定义一个Mysql结构体
-pub struct Mysql {
-    pub host: String,
-}
-
-//Mysql 实现存储层的连接操作
-impl Storage for Mysql {
-    fn connen(&self) {
-        println!("{}", "Mysql 连接成功")
-    }
-}
-
-// 定义一个MongoDB的结构体
-pub struct MongoDB {
-    pub host: String,
-}
-
-impl Storage for MongoDB {
-    fn connen(&self) {
-        println!("{}", "MongoDB 连接成功")
-    }
-}
-
-//创建一个统一的动态数组存储这些连接
-
-pub struct StorageLayer {
-    pub storage_components: Vec<Box<dyn Storage>>,
-}
-
-impl StorageLayer {
-    pub fn run(&self) {
-        for component in self.storage_components.iter() {
-            component.connen()
-        }
-    }
-}
