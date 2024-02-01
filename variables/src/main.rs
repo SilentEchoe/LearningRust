b015e82838f698d59316ebaf342966b0e26f490e
anonymity <kai.wang.info@gmail.com>
2024-02-01 15:48:54 +0800

    可变引用

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,11 @@
 #[warn(unused_imports)]
 
-// 不要修改 main 中的代码
 fn main() {
-    let t = (String::from("hello"), String::from("world"));
-
-    // 填空，不要修改其它代码
-    let (s1 ,s2) = (&t.0 ,&t.1);
-
-    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+    let mut s1 = String::from("hello");
+    change(&mut s1);
+    println!("{}", s1);
 }
 
+fn change(some_string: &mut String)  {
+   some_string.push_str(", world");
+}
