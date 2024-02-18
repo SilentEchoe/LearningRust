bbfcf80bdfa7c7eb3e1101eb8003830539cc9287
anonymity <kai.wang.info@gmail.com>
2024-02-18 17:51:15 +0800

    生命周期

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,33 +1,22 @@
-/* 增加合适的生命周期标准，让代码工作 */
 
-// `i32` 的引用必须比 `Borrowed` 活得更久
 #[derive(Debug)]
-struct Borrowed<'a>(&'a i32);
+struct NoCopyType {}
 
-// 类似的，下面两个引用也必须比结构体 `NamedBorrowed` 活得更久
 #[derive(Debug)]
-struct NamedBorrowed<'a> {
-    x: &'a i32,
-    y: &'a i32,
+#[allow(dead_code)]
+struct Example<'a, 'b> {
+    a: &'a u32,
+    b: &'b NoCopyType
 }
 
-#[derive(Debug)]
-enum Either<'a> {
-    Num(i32),
-    Ref(&'a i32),
-}
+/* 修复函数的签名 */
+fn fix_me<'b>(foo: &'b Example) -> &'b NoCopyType
+{  foo.b }
 
-fn main() {
-    let x = 18;
-    let y = 15;
-
-    let single = Borrowed(&x);
-    let double = NamedBorrowed { x: &x, y: &y };
-    let reference = Either::Ref(&x);
-    let number    = Either::Num(y);
-
-    println!("x is borrowed in {:?}", single);
-    println!("x and y are borrowed in {:?}", double);
-    println!("x is borrowed in {:?}", reference);
-    println!("y is *not* borrowed in {:?}", number);
+fn main()
+{
+    let no_copy = NoCopyType {};
+    let example = Example { a: &1, b: &no_copy };
+    fix_me(&example);
+    println!("Success!")
 }
