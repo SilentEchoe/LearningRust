d7a5c2e6415e94a9835380f19ca4864386f168b8
anonymity <kai.wang.info@gmail.com>
2024-02-19 17:43:31 +0800

    传播错误?

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
+use std::io;
+use std::io::Read;
 
-fn main() {
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => {
-            panic!("Problem opening the file: {:?}", error)
-        },
-    };
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
+
+fn main() {}
