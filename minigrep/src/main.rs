c0b9ba52b001f4a9ac3bba2fb0c7035f78ff2517
anonymity <kai.wang.info@gmail.com>
2024-02-21 18:04:16 +0800

    文件搜索工具

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // env::args 读取并分析传入的命令行参数，最终通过 collect 方法将其转换为一个集合类型 Vector
@@ -9,6 +10,11 @@ fn main() {
     //需要两个变量来存储文件路径和待搜索的字符串
     let query = &args[1];
     let filename = &args[2];
-    println!("Searching for {}", query);
+
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+ println!("With text:\n{}", contents);
 }
