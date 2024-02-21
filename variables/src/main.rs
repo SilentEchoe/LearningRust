5ab62d69ddb657cf431d6f3dc3fbf075e663e14f
anonymity <kai.wang.info@gmail.com>
2024-02-21 16:38:53 +0800

    传播错误?

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,42 +1,19 @@
-use std::fs::File;
-use std::io::{self, Read};
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    // 打开文件，f是`Result<文件句柄,io::Error>`
-    let f = File::open("hello.txt");
+// 填空并修复错误
+use std::num::ParseIntError;
 
-    let mut f = match f {
-        // 打开文件成功，将file句柄赋值给f
-        Ok(file) => file,
-        // 打开文件失败，将错误返回(向上传播)
-        Err(e) => return Err(e),
-    };
-    // 创建动态字符串s
-    let mut s = String::new();
-    // 从f文件句柄读取数据并写入s中
-    match f.read_to_string(&mut s) {
-        // 读取成功，返回Ok封装的字符串
-        Ok(_) => Ok(s),
-        // 将错误向上传播
-        Err(e) => Err(e),
-    }
+fn multiply(n1_str: &str, n2_str: &str) -> Result<i32, ParseIntError>{
+    let n1 = n1_str.parse::<i32>();
+    let n2 = n2_str.parse::<i32>();
+    Ok(n1.unwrap() * n2.unwrap())
 }
 
-fn read_username_from_file_v2() -> Result<String, io::Error> {
-    // 打开文件，f是`Result<文件句柄,io::Error>`
-    let mut f = File::open("hello.txt")?;
-    // 创建动态字符串s
-    let mut s = String::new();
-    // 从f文件句柄读取数据并写入s中
-    f.read_to_string(&mut s)?;
-    // 读取成功，返回Ok封装的字符串
-    Ok(s)
-}
+fn main() {
+    let result = multiply("10", "2");
+    assert_eq!(result, Ok(20));
 
-fn read_username_from_file_v3() -> Result<String, io::Error> {
-    let mut s = String::new();
-    File::open("hello.txt")?.read_to_string(&mut s)?; //链式调用
-    Ok(s)
-}
+    let result = multiply("4", "2");
+    assert_eq!(result.unwrap(), 8);
 
-fn main() {}
+    println!("Success!")
+}
