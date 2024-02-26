9533cab2997e33e5f43b0eb2b0136750d8ea0e6d
anonymity <kai.wang.info@gmail.com>
2024-02-26 14:34:02 +0800

    重构

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,13 +8,34 @@ fn main() {
     dbg!(&args);
 
     //需要两个变量来存储文件路径和待搜索的字符串
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
- println!("With text:\n{}", contents);
+    println!("With text:\n{}", contents);
 }
+
+struct Config{
+     query: String,
+     file_path: String,
+
+}
+
+impl Config{
+  fn new(args: &[String]) -> Config{
+    let query = args[1].clone();
+    let file_path = &args[2].clone();
+    Config{query,file_path}
+  }
+}
+
+
+fn parse_config(args: &[String]) -> Config{
+  let query = args[1].clone();
+  let file_path = &args[2].clone();
+  return Config{query,file_path}
+}
+
