90cdc118627a7d3da5e1168c4f07eae73e78de29
anonymity <kai.wang.info@gmail.com>
2024-02-26 14:46:49 +0800

    重构

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -23,8 +23,6 @@ fn main() {
 
 fn run(config: Config) -> Result<(),Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-
-
     println!("With text:\n{}", contents);
     Ok(())
 }
