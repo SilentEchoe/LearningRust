4a94a7b8479f66d6fdc8c4bde06174c21ccf82ea
anonymity <kai.wang.info@gmail.com>
2024-08-03 23:19:23 +0800

    所有权

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,12 +1,13 @@
-
+// 不要修改 main 中的代码
 fn main() {
-    // 1. 通过引用来避免所有权转移
-    // let x = String::from("hello, world");
-    // let y = &x;
-    // println!("{},{}",x,y);
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
 
-    // 2.深拷贝
-    let x = String::from("hello, world");
-    let y = x.clone();  
-    println!("{},{}",x,y);
+    println!("{}", s2);
+}
+
+// 只能修改下面的代码!
+fn take_ownership(s: String) -> String {
+    println!("{}", s);
+    s
 }
