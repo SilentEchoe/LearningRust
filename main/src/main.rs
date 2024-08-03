79d916fee9f7cb3a7a1f66816a7e27a5a5521143
anonymity <kai.wang.info@gmail.com>
2024-08-04 00:03:52 +0800

    所有权

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,18 +1,12 @@
 
+// 注释掉一行代码让它工作
 fn main() {
-    let c = '中';
+    let mut s = String::from("hello, ");
 
-    let r1 = &c;
-    // 填写空白处，但是不要修改其它行的代码
-    let ref r2 = c;
+    let r1 = &mut s;
+    r1.push_str("world");
+    let r2 = &mut s;
+    r2.push_str("!");
 
-    assert_eq!(*r1, *r2);
-
-    // 判断两个内存地址的字符串是否相等
-    assert_eq!(get_addr(r1),get_addr(r2));
-}
-
-// 获取传入引用的内存地址的字符串形式
-fn get_addr(r: &char) -> String {
-    format!("{:p}", r)
+    println!("{}",r1);
 }
