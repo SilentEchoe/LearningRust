fb1e3631456041ccbad0cd82cf9605893d8489c0
anonymity <kai.wang.info@gmail.com>
2024-08-04 22:03:38 +0800

    结构体

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,15 +1,19 @@
 
-// 填空并修复错误
-struct Color(i32, i32, i32);
-struct Point(i32, i32, i32);
-fn main() {
-    let v = Point(0,127,255);
-    check_color(v);
+// 填空并修复错误，不要增加或移除代码行
+struct Person {
+    name: String,
+    age: u8,
 }
+fn main() {
+    let age = 18;
+    let mut p = Person {
+        name: String::from("sunface"),
+        age,
+    };
 
-fn check_color(p: Point) {
-    let Point(x, _, _) = p;
-    assert_eq!(x, 0);
-    assert_eq!(p.1, 127);
-    assert_eq!(p.2, 255);
+    // how can you believe sunface is only 18? 
+    p.age = 30;
+
+    // 填空
+    p.name = String::from("sunfei");
 }
