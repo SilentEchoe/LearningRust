2b6f9af31b0d548620ada09a4d584e96b6d4d081
anonymity <kai.wang.info@gmail.com>
2024-08-05 11:44:21 +0800

    匹配模式

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,19 +1,25 @@
 
-enum MyEnum {
-    Foo,
-    Bar
+enum Foo {
+    Bar,
+    Baz,
+    Qux(u32)
 }
 
 fn main() {
-    let mut count = 0;
+    let a = Foo::Qux(10);
 
-    let v = vec![MyEnum::Foo,MyEnum::Bar,MyEnum::Foo];
-    for e in v {
-        match e {
-            MyEnum::Foo => count += 1,
-            _ => {}
-        }
-    }
+    // 移除以下代码，使用 `match` 代替
+    // if let Foo::Bar = a {
+    //     println!("match foo::bar")
+    // } else if let Foo::Baz = a {
+    //     println!("match foo::baz")
+    // } else {
+    //     println!("match others")
+    // }
 
-    assert_eq!(count, 2);
+    match a {
+        Foo::Bar => "match foo::bar",
+        Foo::Baz => "match foo::baz",
+        _ => "match others"
+    };
 }
