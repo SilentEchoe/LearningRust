417bd34bde4adbb937bbe1456aa90295ed3b0b44
anonymity <kai.wang.info@gmail.com>
2024-08-05 11:11:34 +0800

    匹配模式

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,19 +1,34 @@
 
 // 填空
-enum Direction {
-    East,
-    West,
-    North,
-    South,
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
-    let dire = Direction::South;
-    match dire {
-        Direction::East => println!("East"),
-        Direction::South  => { // 在这里匹配 South 或 North
-            println!("South or North");
+    let msgs = [
+        Message::Quit,
+        Message::Move{x:1, y:3},
+        Message::ChangeColor(255,255,0)
+    ];
+
+    for msg in msgs {
+        show_message(msg)
+    }
+}
+
+fn show_message(msg: Message) {
+    match msg {
+        Message::Move {x,y} => { // 这里匹配 Message::Move
+            assert_eq!(x, 1);
+            assert_eq!(y, 3);
         },
-        _ => println!("Other"),
-    };
+        Message::ChangeColor(_, g, b) => {
+            assert_eq!(g, 255);
+            assert_eq!(b, 0)
+        }
+        __ => println!("no data in these variants")
+    }
 }
