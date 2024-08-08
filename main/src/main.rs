1c8bde3e65875e4beaef929d26ee6a8f47c1dccd
anonymity <kai.wang.info@gmail.com>
2024-08-08 17:39:28 +0800

    特征对象

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,43 +1,38 @@
-trait Bird {
-    fn quack(&self);
+
+// 填空
+trait Draw {
+    fn draw(&self) -> String;
 }
 
-struct Duck;
-impl Duck {
-    fn fly(&self) {
-        println!("Look, the duck is flying")
-    }
-}
-struct Swan;
-impl Swan {
-    fn fly(&self) {
-        println!("Look, the duck.. oh sorry, the swan is flying")
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
     }
 }
 
-impl Bird for Duck {
-    fn quack(&self) {
-        println!("{}", "duck duck");
-    }
-}
-
-impl Bird for Swan {
-    fn quack(&self) {
-        println!("{}", "swan swan");
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
     }
 }
 
 fn main() {
-    // 填空
-    // let birds = Box::new(Duck);
-    let birds: Vec<Box<Duck>> = Vec::new();
+    let x = 1.1f64;
+    let y = 8u8;
 
+    // draw x
+    draw_with_box(Box::new(x));
 
+    // draw y
+    draw_with_ref(&y);
 
-    for bird in birds {
-        bird.quack();
-        // 当 duck 和 swan 变成 bird 后，它们都忘了如何翱翔于天际，只记得该怎么叫唤了。。
-        // 因此，以下代码会报错
-        // bird.fly();
-    }
+    println!("Success!")
+}
+
+fn draw_with_box(x: Box<dyn Draw>) {
+    x.draw();
+}
+
+fn draw_with_ref(x: &u8) {
+    x.draw();
 }
