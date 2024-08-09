eadc0606783cb1b97a535ce9e082f9cac12cdf63
anonymity <kai.wang.info@gmail.com>
2024-08-09 15:42:57 +0800

    特征默认参数

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,47 +1,23 @@
+use std::ops::Sub;
 
-struct Container(i32, i32);
-
-// 使用关联类型实现重新实现以下特征
-trait Contains {
-   type A;
-   type B;
-
-    fn contains(&self, _:  &Self::A, _: &Self::B) -> bool;
-    fn first(&self) -> i32;
-    fn last(&self) -> i32;
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-impl Contains for Container {
+impl Sub for Point {
+    type Output = Point;
 
-    type A = i32;
-    type B = i32;
-
-    fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
-        (&self.0 == number_1) && (&self.1 == number_2)
+    fn sub(self, other: Point) -> Point {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
     }
-    // Grab the first number.
-    fn first(&self) -> i32 { self.0 }
-
-    // Grab the last number.
-    fn last(&self) -> i32 { self.1 }
-}
-
-
-fn difference<C: Contains>(container: &C) -> i32 {
-    container.last() - container.first()
 }
 
 fn main() {
-    let number_1 = 3;
-    let number_2 = 10;
-
-    let container = Container(number_1, number_2);
-
-    println!("Does container contain {} and {}: {}",
-             &number_1, &number_2,
-             container.contains(&number_1, &number_2));
-    println!("First number: {}", container.first());
-    println!("Last number: {}", container.last());
-
-    println!("The difference is: {}", difference(&container));
+    assert_eq!(Point { x: 1, y: 0 } - Point { x: 2, y: 3 },
+               Point { x: 3, y: 3 });
 }
