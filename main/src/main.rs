7a23ef00964716eff84d006d93fe9e2355d32272
anonymity <kai.wang.info@gmail.com>
2024-08-11 14:54:13 +0800

    特征的继承

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,40 +1,74 @@
-use std::task::Poll;
 
-trait Pilot {
-    fn fly(&self) -> String;
+trait Person {
+    fn name(&self) -> String;
 }
 
-trait Wizard {
-    fn fly(&self) -> String;
+// Person 是 Student 的 supertrait .
+// 实现 Student 需要同时实现 Person.
+trait Student: Person {
+    fn university(&self) -> String;
 }
 
-struct Human;
+trait Programmer {
+    fn fav_language(&self) -> String;
+}
 
-impl Pilot for Human {
-    fn fly(&self) -> String {
-        String::from("This is your captain speaking.")
+// CompSciStudent (computer science student) 是 Programmer
+// 和 Student 的 subtrait. 实现 CompSciStudent 需要先实现这两个 supertraits.
+trait CompSciStudent: Programmer + Student {
+    fn git_username(&self) -> String;
+}
+
+fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+    format!(
+        "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+        student.name(),
+        student.university(),
+        student.fav_language(),
+        student.git_username()
+    )
+}
+
+struct CSStudent {
+    name: String,
+    university: String,
+    fav_language: String,
+    git_username: String
+}
+
+// 为 CSStudent 实现所需的特征
+impl Programmer for CSStudent{
+    fn fav_language(&self) -> String {
+        self.fav_language.clone()
     }
 }
 
-impl Wizard for Human {
-    fn fly(&self) -> String {
-        String::from("Up!")
+impl Person for CSStudent {
+    fn name(&self) -> String {
+       self.name.clone()
     }
 }
 
-impl Human {
-    fn fly(&self) -> String {
-        String::from("*waving arms furiously*")
+impl Student for CSStudent{
+    fn university(&self) -> String{
+        self.university.clone()
+    }
+}
+
+impl CompSciStudent for CSStudent {
+    fn git_username(&self) -> String{
+        self.git_username.clone()
     }
 }
 
 fn main() {
-    let person = Human;
+    let student = CSStudent {
+        name: "Sunfei".to_string(),
+        university: "XXX".to_string(),
+        fav_language: "Rust".to_string(),
+        git_username: "sunface".to_string()
+    };
 
-    assert_eq!(Pilot::fly(&person) , "This is your captain speaking.");
-    assert_eq!(Wizard::fly(&person) , "Up!");
-
-    assert_eq!(Human.fly(), "*waving arms furiously*");
-
-    println!("Success!")
+    // 填空
+    println!("{}", comp_sci_student_greeting(&student));
 }
