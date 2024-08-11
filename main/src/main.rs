3833c4008411e25f2d49b941125592628f42fa6c
anonymity <kai.wang.info@gmail.com>
2024-08-11 21:55:41 +0800

    HashMap

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,28 +1,33 @@
 
-// 填空
+// 修复错误
+// 提示: `derive` 是实现一些常用特征的好办法
 use std::collections::HashMap;
-fn main() {
-    // 编译器可以根据后续的使用情况帮我自动推断出 HashMap 的类型，当然你也可以显式地标注类型：HashMap<&str, u8>
-    let mut player_stats = HashMap::new();
 
-    // 查询指定的 key, 若不存在时，则插入新的 kv 值
-    player_stats.entry("health").or_insert(100);
-
-    assert_eq!(player_stats["health"], 100);
-
-    // 通过函数来返回新的值
-    player_stats.entry("health").or_insert_with(random_stat_buff);
-    assert_eq!(player_stats["health"], 100);
-
-    let health = player_stats.entry("health").or_insert(50);
-    assert_eq!(health,&100);
-    *health -= 50;
-    assert_eq!(*health, 50);
-
-    println!("Success!")
+#[derive(Eq, PartialEq,Hash,Debug)]
+struct Viking {
+    name: String,
+    country: String,
 }
 
-fn random_stat_buff() -> u8 {
-    // 为了简单，我们没有使用随机，而是返回一个固定的值
-    42
+impl Viking {
+    fn new(name: &str, country: &str) -> Viking {
+        Viking {
+            name: name.to_string(),
+            country: country.to_string(),
+        }
+    }
+}
+
+fn main() {
+    // 使用 HashMap 来存储 viking 的生命值
+    let vikings = HashMap::from([
+        (Viking::new("Einar", "Norway"), 25),
+        (Viking::new("Olaf", "Denmark"), 24),
+        (Viking::new("Harald", "Iceland"), 12),
+    ]);
+
+    // 使用 derive 的方式来打印 viking 的当前状态
+    for (viking, health) in &vikings {
+        println!("{:?} has {} hp", viking, health);
+    }
 }
