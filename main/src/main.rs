b6d49eb33968cbb2a15dc3310c3c92f916cd46ea
anonymity <kai.wang.info@gmail.com>
2024-08-11 21:36:34 +0800

    HashMap

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,27 +1,28 @@
 
-// 填空并修复错误
+// 填空
 use std::collections::HashMap;
 fn main() {
-    let mut scores = HashMap::new();
-    scores.insert("Sunface", 98);
-    scores.insert("Daniel", 95);
-    scores.insert("Ashley", 69);
-    scores.insert("Katie", 58);
+    // 编译器可以根据后续的使用情况帮我自动推断出 HashMap 的类型，当然你也可以显式地标注类型：HashMap<&str, u8>
+    let mut player_stats = HashMap::new();
 
-    // get 返回一个 Option<&V> 枚举值
-    let score = scores.get("Sunface").cloned().unwrap_or(0);
-    assert_eq!(score, 98);
+    // 查询指定的 key, 若不存在时，则插入新的 kv 值
+    player_stats.entry("health").or_insert(100);
 
-    if scores.contains_key("Daniel") {
-        // 索引返回一个值 V
-        let score = scores["Daniel"];
-        assert_eq!(score, 95);
-        scores.remove("Daniel");
-    }
+    assert_eq!(player_stats["health"], 100);
 
-    assert_eq!(scores.len(), 3);
+    // 通过函数来返回新的值
+    player_stats.entry("health").or_insert_with(random_stat_buff);
+    assert_eq!(player_stats["health"], 100);
 
-    for (name, score) in scores {
-        println!("The score of {} is {}", name, score)
-    }
+    let health = player_stats.entry("health").or_insert(50);
+    assert_eq!(health,&100);
+    *health -= 50;
+    assert_eq!(*health, 50);
+
+    println!("Success!")
+}
+
+fn random_stat_buff() -> u8 {
+    // 为了简单，我们没有使用随机，而是返回一个固定的值
+    42
 }
