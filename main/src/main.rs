8bcd9d6270f6fae25817e715c78ca055080545eb
anonymity <kai.wang.info@gmail.com>
2024-08-13 16:50:52 +0800

    错误处理

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,17 +1,35 @@
+// 修复所有的 panic，让代码工作
+fn main() {
+   assert_eq!("abc".as_bytes(), [97, 98, 99]);
 
-// 填空
-fn drink(beverage: &str) {
-   if beverage == "lemonade" {
-      println!("Success!");
-      // 实现下面的代码
-      panic!("error")
-   }
+   let v = vec![1, 2, 3,4];
+   let ele = v[3];
+   let ele = v.get(3).unwrap();
 
-   println!("Exercise Failed if printing out this line!");
+   // 大部分时候编译器是可以帮我们提前发现溢出错误，并阻止编译通过。但是也有一些时候，这种溢出问题直到运行期才会出现
+   let _v = production_rate_per_hour(2);
+
+   divide(15, 0);
+
+   println!("Success!")
 }
 
-fn main() {
-   drink("lemonade");
+fn divide(x:u8, y:u8) {
+   if y !=0  {
+      println!("{}", x / y)
+   }
+}
 
-   println!("Exercise Failed if printing out this line!");
+fn production_rate_per_hour(speed: u8) -> f64 {
+   let cph: u8 = 221;
+   match speed {
+      1..=4 => ((speed as f64) * (cph as f64) ) as f64,
+      5..=8 => (speed * cph) as f64 * 0.9,
+      9..=10 => (speed * cph) as f64 * 0.77,
+      _ => 0 as f64,
+   }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+   (production_rate_per_hour(speed) / 60 as f64) as u32
 }
